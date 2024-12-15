c2367de3ab1bb0b75a9a0ce908c83b764ea7ce0b
its-kimna <kimina.uid@gmail.com>
2024-12-15 16:42:44 +0900

    test

--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
-const KEY_API = 'a72b4c6c24bdd6a749c57344f0d562f0';
+const KEY_API = 'd9b3752a8da6dad60c6a60133fbae777';
 
 function initGeo(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY_API}`;
+    console.log(url)
     fetch(url)
     .then((response) => response.json())
     .then((data) =>{
